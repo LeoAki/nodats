b2054caf0d097077095ad45c44935ccb41487019
Leonardo Aquino <leoaki19@gmail.com>
2013-11-27 17:14:50 -0500

    vista corregida

--- describeregistro.sql
+++ describeregistro.sql
@@ -15,7 +15,35 @@ select
 if((`r`.`activo1` = 0),'INACTIVO',if((`r`.`activo1` = 1),'EN PROCESO','COMPLETO')) AS `I`,
 if((`r`.`activo2` = 0),'INACTIVO',if((`r`.`activo2` = 1),'EN PROCESO','COMPLETO')) AS `II`,
 if((`r`.`activo3` = 0),'INACTIVO',if((`r`.`activo3` = 1),'EN PROCESO','COMPLETO')) AS `III`,
-if((`r`.`activo4` = 0),'INACTIVO',if((`r`.`activo4` = 1),'EN PROCESO','COMPLETO')) AS `IV`
+if((`r`.`activo4` = 0),'INACTIVO',if((`r`.`activo4` = 1),'EN PROCESO','COMPLETO')) AS `IV`,
+`se`.`codigo` AS `seccioncode`
+from (((`Registro` `r` join `descripcionseccion` `se` 
+	on((`r`.`codigoseccion` = `se`.`codigo`))) join `Personal_Institucional` `doc` 
+	on((`r`.`codigodocente` = `doc`.`codigo`))) join `Asinatura` `sina` 
+	on((`r`.`codigoasinatura` = `sina`.`codigo`))) 
+
+
+
+
+CREATE  VIEW `describeregistro` 
+AS 
+select 
+`r`.`codigo` AS `codigo`,
+`se`.`grado` AS `grado`,
+`se`.`nombreseccion` AS `nombreseccion`,
+`se`.`nomnivel` AS `nomnivel`,
+`doc`.`paterno` AS `paterno`,
+`doc`.`materno` AS `materno`,
+`doc`.`nombres` AS `nombres`,
+`doc`.`dni` AS `dni`,
+`sina`.`codigo` AS `CODEASINA`,
+`sina`.`asinatura` AS `asinatura`,
+`r`.`codigodocente` AS `iddocente`,
+if((`r`.`activo1` = 0),'INACTIVO',if((`r`.`activo1` = 1),'EN PROCESO','COMPLETO')) AS `I`,
+if((`r`.`activo2` = 0),'INACTIVO',if((`r`.`activo2` = 1),'EN PROCESO','COMPLETO')) AS `II`,
+if((`r`.`activo3` = 0),'INACTIVO',if((`r`.`activo3` = 1),'EN PROCESO','COMPLETO')) AS `III`,
+if((`r`.`activo4` = 0),'INACTIVO',if((`r`.`activo4` = 1),'EN PROCESO','COMPLETO')) AS `IV`,
+`se`.`codigo` AS `seccioncode`
 from (((`Registro` `r` join `descripcionseccion` `se` 
 	on((`r`.`codigoseccion` = `se`.`codigo`))) join `Personal_Institucional` `doc` 
 	on((`r`.`codigodocente` = `doc`.`codigo`))) join `Asinatura` `sina` 
